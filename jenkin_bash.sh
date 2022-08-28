c81efd217f25a38e91b5f4b4beb9f8132343feb4
s3879303 <75520726+s3879303@users.noreply.github.com>
2022-08-28 19:42:44 +0700

    setup jenkins server

---
+++ jenkin_bash.sh
@@ -0,0 +1,10 @@
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo amazon-linux-extras install java-openjdk11 -y
+yum install jenkins -y
+cd /opt
+wget https://dlcdn.apache.org/maven/maven-3/3.8.6/binaries/apache-maven-3.8.6-bin.tar.gz
+tar -xvzf apache-maven-3.8.6-bin.tar.gz
+mv apache-maven-3.8.6 maven
+service jenkins start
+cat /var/lib/jenkins/secrets/initialAdminPassword
